7173058ea3b751c4d51ee401d9d1300a8481585a
quang709 <69231857+quang709@users.noreply.github.com>
2020-09-16 11:18:12 +0700

    Static Property

---
+++ Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static_Property
+{
+   public class Car
+    {
+
+
+        private string name;
+        private string engine;
+        public static int numberOfCars;
+        public Car(string name, string engine)
+        {
+            this.name = name;
+            this.engine = engine;
+            numberOfCars++;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Static_Property
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Car car1 = new Car("Mazda 3", "Skyactiv 3");
+            Console.WriteLine(Car.numberOfCars);
+            Car car2 = new Car("Mazda 6", "Skyactiv 6");
+            Console.WriteLine(Car.numberOfCars);
+            Console.ReadLine();
+        }
+    }
+}
